2037e888255910e2a81a3109fc29dc880eceb640
Kael <i@kael.me>
2016-07-12 17:43:25 +0800

    1.0.0: basic functionalities

---
+++ src/index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const unique = require('array-uniq')
+
+
+function unique_with_filter (array, filter) {
+  let ret = []
+  let i = 0
+  let length = array.length
+
+  for (; i < length; i ++) {
+    if (!contains(ret, array[i], filter)) {
+      ret.push(array[i])
+    }
+  }
+
+  return ret
+}
+
+
+function contains (array, given, filter) {
+  let i = 0
+  let length = array.length
+
+  for (; i < length; i ++) {
+    if (filter(array[i], given)) {
+      return true
+    }
+  }
+}
+
+
+module.exports = (array, filter) => {
+  if (arguments.length === 1) {
+    return unique(array)
+  }
+
+  return unique_with_filter(array, filter)
+}
--- test/make-unique.js
+++ test/make-unique.js
@@ -1,4 +1,24 @@
 'use strict';
 
-var expect = require('chai').expect;
-var make_unique = require('../');
+var expect = require('chai').expect
+var unique = require('../')
+
+
+describe('unique(array, filter)', function () {
+  it('should filter objects', function () {
+    var array = [
+      {name: 1},
+      {name: 2},
+      {name: 1}
+    ]
+
+    var result = unique(array, function (a, b) {
+      return a.name === b.name
+    })
+
+    expect(result).to.deep.equal([
+      {name: 1},
+      {name: 2}
+    ])
+  })
+})
